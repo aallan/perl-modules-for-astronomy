72853680e4fec87af6e72bb4c0ff7453b5c5da80
Alasdair Allan <alasdair@babilim.co.uk>
2003-07-15 20:35:57 +0000

    Documentation patch and fixed test case

---
+++ Astro/SIMBAD/SIMBAD.pm
@@ -0,0 +1,40 @@
+package Astro::SIMBAD;
+
+=head1 NAME
+
+Astro::SIMBAD - An object orientated interface to CDS' SIMBAD database
+
+=head1 DESCRIPTION
+
+This module does nothing, and is here as a placeholder in case of need. The
+user interface to the goodness wrapped in the SIMBAD package can be found in
+the L<Astro::SIMBAD::Query> module.
+
+=head1 REVISION
+
+$Id: SIMBAD.pm,v 1.1 2003/07/15 20:35:57 aa Exp $
+
+=head1 METHODS
+
+The Astro::SIMBAD module itself has no methods, and is just a placeholder
+module, see L<Astro::SIMBAD::Query> for the actual interface.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2001-2003 University of Exeter. All Rights Reserved.
+
+This program was written as part of the eSTAR project and is free software;
+you can redistribute it and/or modify it under the terms of the GNU Public
+License.
+
+=head1 AUTHORS
+
+Alasdair Allan E<lt>aa@astro.ex.ac.ukE<gt>,
+
+=cut
+
+use strict;
+use vars qw/ $VERSION /;
+$VERSION = '1.9.4'
+
+1;                                                                  
