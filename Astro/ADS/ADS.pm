5f785040fe6c78af4946ac07db63a0b530a783dd
Alasdair Allan <alasdair@babilim.co.uk>
2004-01-28 09:04:42 +0000

    minor bug fix

--- Astro/ADS/ADS.pm
+++ Astro/ADS/ADS.pm
@@ -12,7 +12,7 @@ the L<Astro::ADS::Query> module.
 
 =head1 REVISION
 
-$Id: ADS.pm,v 1.2 2003/07/16 18:56:43 aa Exp $
+$Id: ADS.pm,v 1.3 2004/01/28 09:04:42 aa Exp $
 
 =head1 METHODS
 
@@ -35,6 +35,6 @@ Alasdair Allan E<lt>aa@astro.ex.ac.ukE<gt>,
 
 use strict;
 use vars qw/ $VERSION /;
-$VERSION = '1.20.7'
+$VERSION = '1.20.7';
 
 1;                                                                  
