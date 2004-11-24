5f765bc0314d56ddab6d5dc0f93fa2b5ee41bf42
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2004-11-24 01:16:33 +0000

    initial addition, support for object morphology

---
+++ Astro/Catalog/Star/Morphology/Morphology.pm
@@ -0,0 +1,109 @@
+package Astro::Catalog::Star::Morphology;
+
+=head1 NAME
+
+Astro::Catalog::Star::Morphology - Information about a star's morphology.
+
+=head1 SYNOPSIS
+
+  $morphology = new Astro::Catalog::Star::Morphology( );
+
+=head1 DESCRIPTION
+
+Stores information about an astronomical object's morphology.
+
+=cut
+
+use 5.006;
+use strict;
+use warnings;
+use vars qw/ $VERSION /;
+use Carp;
+use Class::Struct;
+
+use warnings::register;
+
+'$Revision: 1.1 $ ' =~ /.*:\s(.*)\s\$/ && ($VERSION = $1);
+
+=head1 METHODS
+
+=head2 Constructor
+
+=over 4
+
+=item B<new>
+
+Create a new instance of an C<Astro::Catalog::Star::Morphology> object.
+
+$morphology = new Astro::Catalog::Star::Morphology( );
+
+This method returns a reference to an C<Astro::Catalog::Star::Morphology>
+object.
+
+=cut
+
+struct ( 'Astro::Catalog::Star::Morphology',
+         { ellipticity => '$',
+           position_angle_pixel => '$',
+           position_angle_world => '$',
+           major_axis_pixel => '$',
+           minor_axis_pixel => '$',
+           major_axis_world => '$',
+           minor_axis_world => '$',
+         }
+       );
+
+=back
+
+=head2 Accessor Methods
+
+=over 4
+
+=item B<ellipticity>
+
+The ellipticity of the object.
+
+=item B<position_angle_pixel>
+
+Position angle using the pixel frame as a reference. Measured counter-
+clockwise from the positive x axis.
+
+=item B<position_angle_world>
+
+Position angle using the world coordinate system as a reference. Measured
+east of north.
+
+=item B<major_axis_pixel>
+
+Length of the semi-major axis in units of pixels.
+
+=item B<minor_axis_pixel>
+
+Length of the semi-minor axis in units of pixels.
+
+=item B<major_axis_world>
+
+Length of the semi-major axis in units of degrees.
+
+=item B<minor_axis_world>
+
+Length of the semi-minor axis in units of degrees.
+
+=back
+
+=head1 REVISION
+
+ $Id: Morphology.pm,v 1.1 2004/11/24 01:16:33 cavanagh Exp $
+
+=head1 COPYRIGHT
+
+Copyright (C) 2004 Particle Physics and Astronomy Research
+Council.  All Rights Reserved.
+
+=head1 AUTHORS
+
+Brad Cavanagh E<lt>b.cavanagh@jach.hawaii.eduE<gt>
+
+=cut
+
+1;
