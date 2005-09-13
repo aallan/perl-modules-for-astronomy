5810cd7a9819a4e43b9f9c8320e20f90b0a04e74
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2005-09-13 02:09:54 +0000

    do not write ~ for ASTR line

--- Astro/Catalog/IO/Astrom.pm
+++ Astro/Catalog/IO/Astrom.pm
@@ -107,7 +107,7 @@ sub _write_catalog {
     $epoch = "2000.0";
   }
 
-  push @output, "~ ASTR";
+  push @output, "ASTR";
   push @output, "~ $ra_cen $dec_cen J2000 $epoch";
 
 # For each star, write the RA, Dec, epoch, X and Y coordinates.
@@ -154,7 +154,7 @@ sub _write_catalog {
 
 =head1 REVISION
 
- $Id: Astrom.pm,v 1.6 2005/06/08 00:45:42 cavanagh Exp $
+ $Id: Astrom.pm,v 1.7 2005/09/13 02:09:54 cavanagh Exp $
 
 =head1 SEE ALSO
 
